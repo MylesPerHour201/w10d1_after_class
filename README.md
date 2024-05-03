193d1c6c69ac5f9c95ad410f005e4fe54ef53fa5
MylesPerHour201 <beck.mj1993@gmail.com>
2024-05-03 14:13:30 -0400

    adding all files to create repo

---
+++ README.md
@@ -0,0 +1,7 @@
+# Bash Scripting Practice and Automation Task 
+
+## In this exercise, we used our knowledge of Bash scripting to work on two tasks. The first was to write a script that will prompt a user to input information, and then display a return message to the user based off their input. The second task was to automate a simple task on your machine using Bash scripting. 
+
+## The biggest challenge that I faced during this exercise was realizing that after created the variable in the script to make a new directory in tast 2, I was not running the "mkdir" command with the variable created. So at first, the script was running successfully, but was not creating the new directory because there was no command being used. Once this was figured out, the script ran and executed perfectly. 
+
+## If using VSCode on Windows, you must make sure that you have the bash language extension installed in oreder to run Bash in the VSCode terminal
---
+++ automation.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+ 
+
+# Automation Task
+
+ 
+
+# Task Description: The automation tast is creating a new directory that will be named for the current date
+
+ 
+
+# Step 1: Identify the repetitive task to be automated
+
+# This task is to create a new directory for today's date. This way any new files created or downloaded can be sorted into this directory. 
+
+ 
+
+# Step 2: Design and implement the automation script
+
+ 
+
+# Placeholder for Bash script implementation
+
+new_directory="$(date)"
+
+mkdir "$new_directory"
+echo "Created directory: $new_directory"
+
+
+# Step 3: Test the automation script
+
+ 
+
+# Placeholder for testing instructions
+
+# To test this script, first save it to a text editor on your machine, such as notepad++, then open your terminal and navigate to the directory where the text file is saved. Make the script executable by using the "chmod +x" command. Run the script in your terminal "./your_text_file.sh". The new directory should be created inside the same directory that stores the text file. You can use the "ls" command to list the contents of the directory to check that the new directory has been created.  
+
+ 
+
+# Step 4: Document the development process
+
+ 
+
+# Placeholder for documentation
+
+# In order to develop this script, I had to narrow down some simple tasks that can be automated on my system. During most work days, a developer will be taking notes, receiving new documentation for current projects, and possible creating new documents as well. Having a concise directory system to be able to sort by date is essential in keeping things orderly and well maintained. Creating a new directory for each day of work is a great way to store files needed for specific dates. Using a bash dictionary as a reference guide on how to create variables and format statements using built in variables as well, the creation of the script was not exceptionally difficult. Using the same bash reference guide, I learned how you have to make files executable in order to actual run the code on a terminal. After making the file executable, using the reference guide I learned the proper syntax to run a script. 
+
+ 
+
+# End of script
+
+
+
+ 
